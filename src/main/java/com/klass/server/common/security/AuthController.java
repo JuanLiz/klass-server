6d009eda8acaa270c8cfe14d2f16ca3a925d41b1
JuanLiz <juand5455@gmail.com>
2023-12-04 23:52:38 -0500

    feat: Added contextPath

--- src/main/java/com/klass/server/common/security/AuthController.java
+++ src/main/java/com/klass/server/common/security/AuthController.java
@@ -24,7 +24,6 @@ public class AuthController {
 
     @PostMapping
     public ResponseEntity authenticate(@RequestBody UserAuth userAuth) {
-        System.out.println("Entering");
         Authentication authenticationToken = new UsernamePasswordAuthenticationToken(
                 userAuth.getEmail(),
                 userAuth.getPassword()
